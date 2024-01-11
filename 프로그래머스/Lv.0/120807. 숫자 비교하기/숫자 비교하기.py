0389ee0f77d5b87689359ce617078763dcf556c9
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-11 19:04:10 +0900

    [level Lv.0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,10 @@
+def solution(num1, num2):
+    if num1==num2 : answer=1
+    else:answer = -1
+    return answer
+num1,num2 = 2,3
+solution(num1, num2)
+num1,num2 = 11,11
+solution(num1, num2)
+num1,num2 = 7,99
+solution(num1, num2)
