81d3fff54dfac4e476bb209a01e95f4a1c8b39cb
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-11 19:02:43 +0900

    [level Lv.0] Title: 두 수의 나눗셈, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120806. 두 수의 나눗셈/두 수의 나눗셈.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    answer = int(num1/num2*1000)
+    return answer
+num1, num2 = 3,2
+solution(num1, num2)
+num1, num2 = 7,3
+solution(num1, num2)
+num1, num2 = 1,16
+solution(num1, num2)
