d0b40ae6c54428cb8fe60efd74f9da2f593f6f57
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-15 21:44:41 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        String sentence;
+        Scanner input = new Scanner(System.in);
+        sentence = input.next();
+        System.out.println(sentence+"??!");
+    }
+}
