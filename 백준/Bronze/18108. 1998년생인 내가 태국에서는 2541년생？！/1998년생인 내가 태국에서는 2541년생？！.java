937c62d33068c15fb2f058e5a1d8ba258cf8d9b2
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-15 22:00:10 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int year;
+        Scanner input = new Scanner(System.in);
+        year = input.nextInt();
+        System.out.println(year-543);
+    }
+}
