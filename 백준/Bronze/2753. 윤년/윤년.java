75ed3a1741a6ecbce5d6eb30e6f45e345e8c4fea
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-15 21:39:06 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        int year;
+        Scanner input=new Scanner(System.in);
+        year = input.nextInt();
+        if (year%400==0){
+            System.out.println(1);
+        }
+        else if (year%4==0) {
+            if (year%100!=0) {
+                System.out.println(1);
+            }
+            else{
+                System.out.println(0);
+            }
+        }    
+
+        else{
+            System.out.println(0);
+        }
+    }
+}
