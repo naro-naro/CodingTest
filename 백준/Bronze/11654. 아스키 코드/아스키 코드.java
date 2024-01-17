53efe5a4aacd40f3f90e5f74b0485bfb32118446
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-18 01:38:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        char ch;
+        Scanner input = new Scanner(System.in);
+        ch = input.next().charAt(0);
+        System.out.println((int)ch);
+    }
+}
