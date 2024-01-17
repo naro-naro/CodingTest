e45ba162d3777d3f2e2673f2f1440b965335982a
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-18 01:32:08 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        String str;
+        int n;
+        Scanner input = new Scanner(System.in);
+        str = input.next();
+        n = str.length();
+        System.out.println(n);
+    }
+}
