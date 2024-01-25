e7685a106d2f1e6314a1d3527d8214af51e4632e
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-25 11:39:04 +0900

    [Bronze V] Title: 직사각형, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int a, b;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.println(a*b);
+    }
+}
